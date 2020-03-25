56447114568eaf756c1399f605928e615ba0b35e
Mehmet Can Taş <Mehmet Can Ta@DESKTOP-JFTQ6KS>
2020-03-25 21:50:48 +0300

    Initial commit

---
+++ NetCoreLanguageDetection/ApiKeyCredential.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+
+namespace NetCoreLanguageDetection
+{
+    public class ApiKeyCredential : ServiceClientCredentials
+    {
+         readonly string apiKey;
+
+        public ApiKeyCredential(string apiKeyParam)
+        {
+            apiKey = apiKeyParam;
+        }
+
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+            // client isteğinin Header bilgilerine API key değerimizi ekliyoruz.
+            request.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ NetCoreLanguageDetection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+
+namespace NetCoreLanguageDetection
+{
+    class Program
+    {
+        private static readonly string apiKey = "c3aff62d4fb345e498575d4c997f5260";
+        private static readonly string apiEndpoint = "https://azuretextanalyitcstest.cognitiveservices.azure.com/";
+        
+        static void Main(string[] args)
+        {
+            var client =  AuthenticateClient();
+
+            Console.Write("Write something : ");
+
+            var userInput = Console.ReadLine();
+            
+            var result = client.DetectLanguage(userInput,"tr");
+            Console.WriteLine($"Detected Language: {result.DetectedLanguages[0].Name}");
+
+            Console.ReadLine();
+        }
+        
+        
+        static TextAnalyticsClient AuthenticateClient()
+        {
+            var credentials = new ApiKeyCredential(apiKey);
+            var client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = apiEndpoint
+            };
+            return client;
+        }
+    }
+}
